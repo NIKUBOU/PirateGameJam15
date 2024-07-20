274d9356571db197e5727f50e3bc34e7e8ac5a79
TheKarmatic <113950363+TheKarmatic@users.noreply.github.com>
2024-07-20 19:14:54 -0400

    Added comments for ease of understanding

--- Assets/Scripts/Gridding/GridManager.cs
+++ Assets/Scripts/Gridding/GridManager.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    [SerializeField] private int gridWidth, gridHeight;
+    [Header("Grid Layout")]
+
+    [Tooltip("Keep your aspect ratio in mind (go with multiples of 16)")]
+    [SerializeField] private int gridWidth;
+
+    [Tooltip("Keep your aspect ratio in mind (go with multiples of 9)")]
+    [SerializeField] private int gridHeight;
     [SerializeField] private Tile tilePrefab;
+
+    [Header("Camera control")]
     [SerializeField] private Transform cam;
     [SerializeField] private bool centerCamToGrid;
 
@@ -16,18 +24,22 @@ public class GridManager : MonoBehaviour
 
     void GenerateGrid()
     {
+        //Takes in the height and width values to generated a grid with that many rows and columns
         for (int x = 0; x < gridWidth; x++)
         {
             for (int z = 0; z < gridHeight; z++)
             {
+                //Spawns a grid tile
                 var spawnedTile = Instantiate(tilePrefab, new Vector3(x, 0, z), Quaternion.identity);
                 spawnedTile.name = $"Tile {x} {z}";
 
+                //This part is just to cycle between the base and the offset colors to get a grid pattern (can be removed as needed)
                 var isOffset = (x % 2 == 0 && z % 2 != 0) || (x % 2 != 0 && z % 2 == 0);
                 spawnedTile.Init(isOffset);
             }
         }
 
+        //Moves the camera to the center of the grid if needed
         if (centerCamToGrid)
         {
             cam.transform.position = new Vector3 ((float)gridWidth/2 - .5f, 10, (float)gridHeight/2 - .5f);
--- Assets/Scripts/Gridding/Tile.cs
+++ Assets/Scripts/Gridding/Tile.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    [Header("Coloring")]
     [SerializeField] private Material baseColor, offsetColor;
 
     public void Init(bool isOffset)
     {
+        //Fetches the meshRenderer of the tile to edit it's color
         MeshRenderer mR = GetComponent<MeshRenderer>();
 
+        //Applies the colors
         if (isOffset)
         {
             mR.material = offsetColor;
