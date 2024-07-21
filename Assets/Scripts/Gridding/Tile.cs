aaf9d28276da714c47ecb25090c6bac831d8fbfb
TheKarmatic <113950363+TheKarmatic@users.noreply.github.com>
2024-07-21 18:59:09 -0400

    Added city growth

--- Assets/Scripts/Gridding/Tile.cs
+++ Assets/Scripts/Gridding/Tile.cs
@@ -10,39 +10,46 @@ public class Tile : MonoBehaviour
     [SerializeField] private Material offsetColor;
     [SerializeField] private Material cityBaseColor;
     [SerializeField] private Material cityOffsetColor;
+    private MeshRenderer mR;
 
     private bool offsetTile;
+    private int xID;
+    private int zID;
+    public int XID { get { return xID; } }
+    public int ZID { get { return zID; } }
 
-    public void Init(bool isOffset, bool isCity)
+    public void Init(bool isOffset, bool isCity, int _xID, int _zID)
     {
         //Fetches the meshRenderer of the tile to edit it's color
-        MeshRenderer mR = GetComponent<MeshRenderer>();
+        mR = GetComponent<MeshRenderer>();
 
         //Applies the colors
-        if (isOffset)
+        if (isCity)
         {
-            if (isCity)
-            {
-                mR.material = cityOffsetColor;
-            }
-            else
-            {
-                mR.material = offsetColor;
-            }
+            mR.material = isOffset ? cityOffsetColor : cityBaseColor;
         }
         else
         {
-            if (isCity)
-            {
-                mR.material = cityBaseColor;
-            }
-            else
-            {
-                mR.material = baseColor;
-            }
+            mR.material = isOffset ? offsetColor : baseColor;
         }
 
         //Retains the offset factor for coloring down the line
         offsetTile = isOffset;
+
+        //Remembers the tile's ID
+        xID = _xID;
+        zID = _zID;
+    }
+
+    public void Colonize()
+    {
+        if (offsetTile)
+        {
+            mR.material = cityOffsetColor;
+        }
+        else
+        {
+            mR.material = cityBaseColor;
+        }
     }
 }
